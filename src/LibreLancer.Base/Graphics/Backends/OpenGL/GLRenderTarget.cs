172b88d7c704b7ce58af362d0fdabec8d2162215
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-04-09 10:58:57 +0300

    Auto-update headers via pre_commit.ps1 - 2026-04-09

--- src/LibreLancer.Base/Graphics/Backends/OpenGL/GLRenderTarget.cs
+++ src/LibreLancer.Base/Graphics/Backends/OpenGL/GLRenderTarget.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky and Librelancer Contributors
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 08 апреля 2026 14:27:48
- * Version: 1.0.2426
+ * Last Updated: 09 апреля 2026 10:58:14
+ * Version: 1.0.2427
  */
 
 namespace LibreLancer.Graphics.Backends.OpenGL;
