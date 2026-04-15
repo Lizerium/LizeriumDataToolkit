0d591a54e8322869468b17bfc8b4d7e33a37b935
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-04-15 06:53:07 +0300

    Auto-update headers via pre_commit.ps1 - 2026-04-15

--- src/LibreLancer.Base/Collections/ConcurrentHashSet.cs
+++ src/LibreLancer.Base/Collections/ConcurrentHashSet.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky and Librelancer Contributors
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 14 апреля 2026 12:25:20
- * Version: 1.0.2432
+ * Last Updated: 15 апреля 2026 06:52:33
+ * Version: 1.0.2436
  */
 
 using System.Collections;
