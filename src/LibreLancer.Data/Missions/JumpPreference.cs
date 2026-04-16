7e7f66d07dc79efb3971a6dd04d973cd45b3c4ac
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-04-16 11:44:56 +0300

    Auto-update headers via pre_commit.ps1 - 2026-04-16

--- src/LibreLancer.Data/Missions/JumpPreference.cs
+++ src/LibreLancer.Data/Missions/JumpPreference.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky and Librelancer Contributors
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 15 апреля 2026 06:52:33
- * Version: 1.0.2436
+ * Last Updated: 16 апреля 2026 11:44:13
+ * Version: 1.0.2437
  */
 
 namespace LibreLancer.Data.Missions;
