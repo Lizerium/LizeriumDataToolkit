c3ad3dae4b42fd3b62b588bb59b52a774b934d7e
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-04-17 06:52:41 +0300

    Auto-update headers via pre_commit.ps1 - 2026-04-17

--- src/LibreLancer.Base/Graphics/Backends/Null/NullRenderTarget2D.cs
+++ src/LibreLancer.Base/Graphics/Backends/Null/NullRenderTarget2D.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky and Librelancer Contributors
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 16 апреля 2026 11:44:13
- * Version: 1.0.2437
+ * Last Updated: 17 апреля 2026 06:52:10
+ * Version: 1.0.2441
  */
 
 using System.Threading.Tasks;
