cc241a134ca9310334ad82907ba1b3b43a7b8b4b
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-04-20 16:23:07 +0300

    Auto-update headers via pre_commit.ps1 - 2026-04-20

--- src/LibreLancer.Data/Missions/EmpathyIni.cs
+++ src/LibreLancer.Data/Missions/EmpathyIni.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky and Librelancer Contributors
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 20 апреля 2026 03:07:11
- * Version: 1.0.2443
+ * Last Updated: 20 апреля 2026 16:22:24
+ * Version: 1.0.2444
  */
 
 using System.Collections.Generic;
