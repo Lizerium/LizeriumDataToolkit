999f1b50e35e7827c95b7acde109947bd904cb6c
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-04-26 09:57:30 +0300

    Auto-update headers via pre_commit.ps1 - 2026-04-26

--- src/LibreLancer.Base/Graphics/Backends/IRenderTarget2D.cs
+++ src/LibreLancer.Base/Graphics/Backends/IRenderTarget2D.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky and Librelancer Contributors
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 25 апреля 2026 08:11:37
- * Version: 1.0.2450
+ * Last Updated: 26 апреля 2026 09:57:02
+ * Version: 1.0.2451
  */
 
 namespace LibreLancer.Graphics.Backends;
