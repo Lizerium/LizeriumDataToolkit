3b13fedb6a99aaa31d7a2ab977527815b63d9ec9
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-04-28 14:27:09 +0300

    Auto-update headers via pre_commit.ps1 - 2026-04-28

--- src/LibreLancer.Data/Universe/DensityRestriction.cs
+++ src/LibreLancer.Data/Universe/DensityRestriction.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky and Librelancer Contributors
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 27 апреля 2026 09:42:08
- * Version: 1.0.2452
+ * Last Updated: 28 апреля 2026 14:26:25
+ * Version: 1.0.2453
  */
 
 namespace LibreLancer.Data.Universe;
