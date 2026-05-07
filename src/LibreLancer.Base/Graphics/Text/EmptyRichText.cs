b745851bb41e55e91454eb9ae55378a57c13a0a3
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-05-07 15:28:46 +0300

    Auto-update headers via pre_commit.ps1 - 2026-05-07

--- src/LibreLancer.Base/Graphics/Text/EmptyRichText.cs
+++ src/LibreLancer.Base/Graphics/Text/EmptyRichText.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky and Librelancer Contributors
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 06 мая 2026 10:30:31
- * Version: 1.0.2467
+ * Last Updated: 07 мая 2026 15:28:01
+ * Version: 1.0.2468
  */
 
 namespace LibreLancer.Graphics.Text;
