32cd5166bcb15ca9318efe005d89729b16ec3681
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-05-22 11:41:41 +0300

    Auto-update headers via pre_commit.ps1 - 2026-05-22

--- src/LibreLancer.Data/Universe/BaseInfo.cs
+++ src/LibreLancer.Data/Universe/BaseInfo.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky and Librelancer Contributors
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 21 мая 2026 11:39:11
- * Version: 1.0.2482
+ * Last Updated: 22 мая 2026 11:40:54
+ * Version: 1.0.2483
  */
 
 using LibreLancer.Data.Ini;
