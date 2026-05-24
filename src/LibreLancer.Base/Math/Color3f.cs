1c9223033a7d45ee5876f6a7cc5a85fee1ca1182
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-05-24 06:53:57 +0300

    Auto-update headers via pre_commit.ps1 - 2026-05-24

--- src/LibreLancer.Base/Math/Color3f.cs
+++ src/LibreLancer.Base/Math/Color3f.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky and Librelancer Contributors
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 23 мая 2026 11:14:24
- * Version: 1.0.2484
+ * Last Updated: 24 мая 2026 06:53:11
+ * Version: 1.0.2485
  */
 
 // MIT License - Copyright (c) Callum McGing
