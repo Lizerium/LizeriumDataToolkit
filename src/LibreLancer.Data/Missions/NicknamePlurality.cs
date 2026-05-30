46be9e779e47868c8fb071f4c6f4d66002b08e72
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-05-30 15:44:33 +0300

    Auto-update headers via pre_commit.ps1 - 2026-05-30

--- src/LibreLancer.Data/Missions/NicknamePlurality.cs
+++ src/LibreLancer.Data/Missions/NicknamePlurality.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky and Librelancer Contributors
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 29 мая 2026 13:55:42
- * Version: 1.0.2490
+ * Last Updated: 30 мая 2026 15:43:52
+ * Version: 1.0.2491
  */
 
 namespace LibreLancer.Data.Missions
