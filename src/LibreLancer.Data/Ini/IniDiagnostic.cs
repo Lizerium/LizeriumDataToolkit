24d196a15ec3f450c635e354d428056e534a907f
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-06-05 06:53:55 +0300

    Auto-update headers via pre_commit.ps1 - 2026-06-05

--- src/LibreLancer.Data/Ini/IniDiagnostic.cs
+++ src/LibreLancer.Data/Ini/IniDiagnostic.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky and Librelancer Contributors
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 04 июня 2026 06:53:13
- * Version: 1.0.2496
+ * Last Updated: 05 июня 2026 06:53:10
+ * Version: 1.0.2497
  */
 
 namespace LibreLancer.Data.Ini;
