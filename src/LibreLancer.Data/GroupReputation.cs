4a20fa8ae004a0404d2dfd3ce0cb5ef4f8a98905
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-06-15 06:54:00 +0300

    Auto-update headers via pre_commit.ps1 - 2026-06-15

--- src/LibreLancer.Data/GroupReputation.cs
+++ src/LibreLancer.Data/GroupReputation.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky and Librelancer Contributors
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 14 июня 2026 16:36:05
- * Version: 1.0.2506
+ * Last Updated: 15 июня 2026 06:53:14
+ * Version: 1.0.2507
  */
 
 namespace LibreLancer.Data
