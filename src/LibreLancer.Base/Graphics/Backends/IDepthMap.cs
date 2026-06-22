5af6161a2f1c0b186905f148be69e262b684d495
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-06-22 06:54:13 +0300

    Auto-update headers via pre_commit.ps1 - 2026-06-22

--- src/LibreLancer.Base/Graphics/Backends/IDepthMap.cs
+++ src/LibreLancer.Base/Graphics/Backends/IDepthMap.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky and Librelancer Contributors
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 21 июня 2026 06:53:04
- * Version: 1.0.2513
+ * Last Updated: 22 июня 2026 06:53:25
+ * Version: 1.0.2514
  */
 
 namespace LibreLancer.Graphics.Backends;
