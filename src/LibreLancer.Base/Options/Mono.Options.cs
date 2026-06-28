ce589db012b478447234a34424c88bfa95bf3e4a
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-06-28 11:44:44 +0300

    Auto-update headers via pre_commit.ps1 - 2026-06-28

--- src/LibreLancer.Base/Options/Mono.Options.cs
+++ src/LibreLancer.Base/Options/Mono.Options.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky and Librelancer Contributors
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 27 июня 2026 13:14:43
- * Version: 1.0.2519
+ * Last Updated: 28 июня 2026 11:43:49
+ * Version: 1.0.2520
  */
 
 //
