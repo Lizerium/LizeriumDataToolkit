a67be9dc1c38a3d72f5c8e02d4b94ca9676c26e1
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-07-03 08:31:06 +0300

    Auto-update headers via pre_commit.ps1 - 2026-07-03

--- src/LibreLancer.Data/Missions/MissionDialog.cs
+++ src/LibreLancer.Data/Missions/MissionDialog.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky and Librelancer Contributors
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 02 июля 2026 07:15:12
- * Version: 1.0.2524
+ * Last Updated: 03 июля 2026 08:30:18
+ * Version: 1.0.2525
  */
 
 // MIT License - Copyright (c) Callum McGing
