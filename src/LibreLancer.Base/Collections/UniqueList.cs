c4a75377bea1df741ff25bf3eccba523b1164fb7
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-07-04 08:48:31 +0300

    Auto-update headers via pre_commit.ps1 - 2026-07-04

--- src/LibreLancer.Base/Collections/UniqueList.cs
+++ src/LibreLancer.Base/Collections/UniqueList.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky and Librelancer Contributors
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 03 июля 2026 08:30:18
- * Version: 1.0.2525
+ * Last Updated: 04 июля 2026 08:47:56
+ * Version: 1.0.2526
  */
 
 using System.Collections;
