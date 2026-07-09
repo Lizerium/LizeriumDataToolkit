94b1790e6e186e05c789f04f73a61995abebcf77
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-07-09 07:03:12 +0300

    Auto-update headers via pre_commit.ps1 - 2026-07-09

--- src/LibreLancer.Data/IO/VfsItem.cs
+++ src/LibreLancer.Data/IO/VfsItem.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky and Librelancer Contributors
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 08 июля 2026 07:27:53
- * Version: 1.0.2530
+ * Last Updated: 09 июля 2026 07:02:22
+ * Version: 1.0.2531
  */
 
 namespace LibreLancer.Data.IO;
