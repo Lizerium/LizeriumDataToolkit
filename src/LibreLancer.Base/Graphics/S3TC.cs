6bab926d145db0784782f0066da52726abd169d8
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-07-10 11:56:28 +0300

    Auto-update headers via pre_commit.ps1 - 2026-07-10

--- src/LibreLancer.Base/Graphics/S3TC.cs
+++ src/LibreLancer.Base/Graphics/S3TC.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky and Librelancer Contributors
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 09 июля 2026 07:02:22
- * Version: 1.0.2531
+ * Last Updated: 10 июля 2026 11:55:37
+ * Version: 1.0.2532
  */
 
 /*
