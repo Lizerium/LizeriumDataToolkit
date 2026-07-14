cc8044be3d88f68148c3e2329fd50891c67a0bf5
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-07-14 06:54:42 +0300

    Auto-update headers via pre_commit.ps1 - 2026-07-14

--- src/LibreLancer.Data/Cameras/CameraIni.cs
+++ src/LibreLancer.Data/Cameras/CameraIni.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky and Librelancer Contributors
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 13 июля 2026 06:53:10
- * Version: 1.0.2535
+ * Last Updated: 14 июля 2026 06:53:44
+ * Version: 1.0.2536
  */
 
 // MIT License - Copyright (c) Callum McGing
