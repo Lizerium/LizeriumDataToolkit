2d4dcc88ec8e6ec455cbbe5cf21080ed2be47b5b
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-07-21 13:32:09 +0300

    Auto-update headers via pre_commit.ps1 - 2026-07-21

--- src/LibreLancer.Data.Generator/IniTypeInfo.cs
+++ src/LibreLancer.Data.Generator/IniTypeInfo.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky and Librelancer Contributors
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 20 июля 2026 12:10:56
- * Version: 1.0.2542
+ * Last Updated: 21 июля 2026 13:31:23
+ * Version: 1.0.2543
  */
 
 using LibreLancer.GeneratorCommon;
