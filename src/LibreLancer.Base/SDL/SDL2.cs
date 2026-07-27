4377f9e8bf688b278596bae19ad3c3973cac2a95
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-07-27 14:58:19 +0300

    Auto-update headers via pre_commit.ps1 - 2026-07-27

--- src/LibreLancer.Base/SDL/SDL2.cs
+++ src/LibreLancer.Base/SDL/SDL2.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky and Librelancer Contributors
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 26 июля 2026 15:12:08
- * Version: 1.0.2548
+ * Last Updated: 27 июля 2026 14:57:34
+ * Version: 1.0.2549
  */
 
 #region License
