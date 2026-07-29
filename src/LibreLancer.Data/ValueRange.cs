6e884bb8d33795592dd8604d7152dac490233c9b
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-07-29 15:46:42 +0300

    Auto-update headers via pre_commit.ps1 - 2026-07-29

--- src/LibreLancer.Data/ValueRange.cs
+++ src/LibreLancer.Data/ValueRange.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky and Librelancer Contributors
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 28 июля 2026 10:11:00
- * Version: 1.0.2550
+ * Last Updated: 29 июля 2026 15:45:53
+ * Version: 1.0.2551
  */
 
 namespace LibreLancer.Data
