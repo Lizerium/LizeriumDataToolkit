6829b0f47ee0fab1a742ae5f1160b1d8bfe2de72
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-07-31 16:28:32 +0300

    Auto-update headers via pre_commit.ps1 - 2026-07-31

--- src/LibreLancer.Data/IO/ZipStructs.cs
+++ src/LibreLancer.Data/IO/ZipStructs.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky and Librelancer Contributors
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 30 июля 2026 06:53:04
- * Version: 1.0.2552
+ * Last Updated: 31 июля 2026 16:27:43
+ * Version: 1.0.2553
  */
 
 using System.Runtime.InteropServices;
