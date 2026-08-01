c96152c97d26896b7b4edcc76bf5a664341c98e6
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-08-01 06:54:37 +0300

    Auto-update headers via pre_commit.ps1 - 2026-08-01

--- src/LibreLancer.Base/Graphics/Backends/GraphicsState.cs
+++ src/LibreLancer.Base/Graphics/Backends/GraphicsState.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky and Librelancer Contributors
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 31 июля 2026 16:27:43
- * Version: 1.0.2553
+ * Last Updated: 01 августа 2026 06:53:40
+ * Version: 1.0.2554
  */
 
 using System.Numerics;
