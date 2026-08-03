9931d0ef9965e58245076db237703f345afa4ed5
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-08-03 06:54:15 +0300

    Auto-update headers via pre_commit.ps1 - 2026-08-03

--- src/LibreLancer.Base/Platforms/Linux/LibFontConfig.cs
+++ src/LibreLancer.Base/Platforms/Linux/LibFontConfig.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky and Librelancer Contributors
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 02 августа 2026 06:53:20
- * Version: 1.0.2555
+ * Last Updated: 03 августа 2026 06:53:24
+ * Version: 1.0.2556
  */
 
 using System;
