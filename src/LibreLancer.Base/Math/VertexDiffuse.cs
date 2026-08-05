7046edb3e81543af263512f15a7175087bec636d
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-08-05 06:53:59 +0300

    Auto-update headers via pre_commit.ps1 - 2026-08-05

--- src/LibreLancer.Base/Math/VertexDiffuse.cs
+++ src/LibreLancer.Base/Math/VertexDiffuse.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky and Librelancer Contributors
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 04 августа 2026 06:53:45
- * Version: 1.0.2557
+ * Last Updated: 05 августа 2026 06:53:12
+ * Version: 1.0.2558
  */
 
 using System.Numerics;
