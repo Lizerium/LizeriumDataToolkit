9eb88f25ee7236e84213c14bd8a470bdec7813a9
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-08-09 15:32:49 +0300

    Auto-update headers via pre_commit.ps1 - 2026-08-09

--- src/LibreLancer.Data/BaseNavBarIni.cs
+++ src/LibreLancer.Data/BaseNavBarIni.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky and Librelancer Contributors
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 08 августа 2026 06:53:20
- * Version: 1.0.2561
+ * Last Updated: 09 августа 2026 15:32:01
+ * Version: 1.0.2562
  */
 
 // MIT License - Copyright (c) Callum McGing
