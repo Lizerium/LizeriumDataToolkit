af26344cdac27d092b4298bc18ba96bce8234278
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-08-10 06:54:04 +0300

    Auto-update headers via pre_commit.ps1 - 2026-08-10

--- src/LibreLancer.Base/AppHandler.cs
+++ src/LibreLancer.Base/AppHandler.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky and Librelancer Contributors
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 09 августа 2026 15:32:01
- * Version: 1.0.2562
+ * Last Updated: 10 августа 2026 06:53:17
+ * Version: 1.0.2563
  */
 
 using System;
