b4ae48b294d784320abb7a2a3c2f81adea97fbd9
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-08-19 10:24:01 +0300

    Auto-update headers via pre_commit.ps1 - 2026-08-19

--- src/LibreLancer.Data/Dll/BinaryResource.cs
+++ src/LibreLancer.Data/Dll/BinaryResource.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky and Librelancer Contributors
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 18 августа 2026 06:53:24
- * Version: 1.0.2571
+ * Last Updated: 19 августа 2026 10:23:12
+ * Version: 1.0.2572
  */
 
 namespace LibreLancer.Dll;
