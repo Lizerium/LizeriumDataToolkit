50a2656c5be3466493223657e2d130728b6addaf
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-08-24 06:54:11 +0300

    Auto-update headers via pre_commit.ps1 - 2026-08-24

--- src/LibreLancer.Data/Equipment/EquipmentIni.cs
+++ src/LibreLancer.Data/Equipment/EquipmentIni.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky and Librelancer Contributors
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 23 августа 2026 06:53:25
- * Version: 1.0.2576
+ * Last Updated: 24 августа 2026 06:53:20
+ * Version: 1.0.2577
  */
 
 // MIT License - Copyright (c) Malte Callum McGing
