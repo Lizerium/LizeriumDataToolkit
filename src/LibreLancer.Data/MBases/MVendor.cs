e3477759e5fc942d09384ff6f0dc8dfc3147aef9
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-08-27 08:34:10 +0300

    Auto-update headers via pre_commit.ps1 - 2026-08-27

--- src/LibreLancer.Data/MBases/MVendor.cs
+++ src/LibreLancer.Data/MBases/MVendor.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky and Librelancer Contributors
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 26 августа 2026 06:53:24
- * Version: 1.0.2579
+ * Last Updated: 27 августа 2026 08:33:31
+ * Version: 1.0.2580
  */
 
 using System.Numerics;
