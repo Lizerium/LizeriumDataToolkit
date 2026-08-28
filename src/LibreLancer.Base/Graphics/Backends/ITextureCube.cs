b40f497d2883899f99750bb9b89592a81c05ac76
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-08-28 06:54:01 +0300

    Auto-update headers via pre_commit.ps1 - 2026-08-28

--- src/LibreLancer.Base/Graphics/Backends/ITextureCube.cs
+++ src/LibreLancer.Base/Graphics/Backends/ITextureCube.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky and Librelancer Contributors
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 27 августа 2026 08:33:31
- * Version: 1.0.2580
+ * Last Updated: 28 августа 2026 06:53:12
+ * Version: 1.0.2581
  */
 
 namespace LibreLancer.Graphics.Backends;
