8637724a94f072127d34beaf6169ab517e67a5b1
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-09-01 08:33:43 +0300

    Auto-update headers via pre_commit.ps1 - 2026-09-01

--- src/LibreLancer.Data/Solar/CollisionGroup.cs
+++ src/LibreLancer.Data/Solar/CollisionGroup.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky and Librelancer Contributors
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 31 августа 2026 06:52:51
- * Version: 1.0.2584
+ * Last Updated: 01 сентября 2026 08:32:52
+ * Version: 1.0.2585
  */
 
 // MIT License - Copyright (c) Malte Rupprecht
