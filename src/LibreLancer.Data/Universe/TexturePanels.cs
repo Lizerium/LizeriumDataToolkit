18523407564ce3e0bce6d17ece23fa3c8ef3fc2d
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-09-07 08:17:59 +0300

    Auto-update headers via pre_commit.ps1 - 2026-09-07

--- src/LibreLancer.Data/Universe/TexturePanels.cs
+++ src/LibreLancer.Data/Universe/TexturePanels.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky and Librelancer Contributors
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 06 сентября 2026 11:08:56
- * Version: 1.0.2590
+ * Last Updated: 07 сентября 2026 08:17:09
+ * Version: 1.0.2591
  */
 
 // MIT License - Copyright (c) Malte Rupprecht
