60bfbfc4cc920e0edc427359dbf64769b32c18f7
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-09-11 06:53:58 +0300

    Auto-update headers via pre_commit.ps1 - 2026-09-11

--- src/LizeriumDataToolkit.Data.TestApp/Program.cs
+++ src/LizeriumDataToolkit.Data.TestApp/Program.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky and Librelancer Contributors
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 10 сентября 2026 10:08:02
- * Version: 1.0.2594
+ * Last Updated: 11 сентября 2026 06:53:12
+ * Version: 1.0.2595
  */
 
 using LibreLancer.Data;
