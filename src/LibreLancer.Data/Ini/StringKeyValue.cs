669f4bebc02eb026027275e66720a0d770a4b4b2
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-09-19 08:47:27 +0300

    Auto-update headers via pre_commit.ps1 - 2026-09-19

--- src/LibreLancer.Data/Ini/StringKeyValue.cs
+++ src/LibreLancer.Data/Ini/StringKeyValue.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky and Librelancer Contributors
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 18 сентября 2026 08:14:39
- * Version: 1.0.2602
+ * Last Updated: 19 сентября 2026 08:46:38
+ * Version: 1.0.2603
  */
 
 // MIT License - Copyright (c) Callum McGing
