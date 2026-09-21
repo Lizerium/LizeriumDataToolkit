28e0ad1de01cc9e3760c42d02ed2c6dce7ba3baa
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-09-21 09:36:21 +0300

    Auto-update headers via pre_commit.ps1 - 2026-09-21

--- src/LibreLancer.Base/Collections/StructEnumerable.cs
+++ src/LibreLancer.Base/Collections/StructEnumerable.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky and Librelancer Contributors
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 20 сентября 2026 08:44:43
- * Version: 1.0.2604
+ * Last Updated: 21 сентября 2026 09:35:31
+ * Version: 1.0.2605
  */
 
 using System.Collections;
