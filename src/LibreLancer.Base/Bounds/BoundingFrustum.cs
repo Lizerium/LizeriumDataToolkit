798f2a276650c460d6b11d061e6ecaf60608dd52
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-09-22 09:19:59 +0300

    Auto-update headers via pre_commit.ps1 - 2026-09-22

--- src/LibreLancer.Base/Bounds/BoundingFrustum.cs
+++ src/LibreLancer.Base/Bounds/BoundingFrustum.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky and Librelancer Contributors
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 21 сентября 2026 09:35:31
- * Version: 1.0.2605
+ * Last Updated: 22 сентября 2026 09:19:10
+ * Version: 1.0.2606
  */
 
 
